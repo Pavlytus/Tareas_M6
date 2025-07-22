a73335c9ff5496f7c659678580ee22e37987dafb
Pavlytus <pavlo.6cn@gmail.com>
2025-07-22 11:23:48 -0600

    Tarea_02_M6

---
+++ Tarea_02_M6/listacompras/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function ListaCompras() {
+  const [productos, setProductos] = useState([]);
+  const [nuevoProducto, setNuevoProducto] = useState("");
+
+  const agregarProducto = () => {
+    if (nuevoProducto.trim() !== "") {
+      setProductos((prev) => [...prev, nuevoProducto.trim()]);
+      setNuevoProducto("");
+    }
+  };
+
+  const eliminarProducto = (indexAEliminar) => {
+    setProductos((prev) => prev.filter((_, idx) => idx !== indexAEliminar));
+  };
+
+  return (
+    <div>
+      <h2>Lista de Compras</h2>
+      <input
+        type="text"
+        value={nuevoProducto}
+        onChange={(e) => setNuevoProducto(e.target.value)}
+      />
+      <button onClick={agregarProducto}>Agregar</button>
+      <ul>
+        {productos.map((producto, index) => (
+          <li key={index}>
+            {producto}{" "}
+            <button onClick={() => eliminarProducto(index)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListaCompras;
